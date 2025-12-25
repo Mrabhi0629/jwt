770a525a06028f0763fdb8eef247d009719aa25b
Ashish singh Goutam <ashishgautam7696@gmail.com>
2025-12-26 00:46:48 +0530

    add user route

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-ECHO is on.
+import express from "express"
+import connectDB from "./services/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+connectDB();
+
+const app = express();
+const port = process.env.PORT;
+app.listen(port,() => {
+    console.log(`you are in the port ${port}`)
+})
